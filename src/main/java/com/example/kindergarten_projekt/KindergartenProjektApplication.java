6223a527208f0b494165366e27438cbaed80c6af
ousmane sow <thifuge@gmail.com>
2025-03-03 14:36:13 +0100

    Springboot initialisiert

---
+++ src/main/java/com/example/kindergarten_projekt/KindergartenProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.kindergarten_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kindergarten_projekt/KindergartenProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kindergarten_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergartenProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
