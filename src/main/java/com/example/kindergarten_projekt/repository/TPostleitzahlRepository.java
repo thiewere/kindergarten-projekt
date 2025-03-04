6765b139dddb89b6cffcafc381c1864bf4065535
EVGENY KVEST <evgenykvest@gmail.com>
2025-03-04 09:37:22 +0100

    TPostleitzahlRepository, TStandortRepository, TZahlungsart erstellt

---
+++ src/main/java/com/example/kindergarten_projekt/repository/TPostleitzahlRepository.java
@@ -0,0 +1,8 @@
+package com.example.kindergarten_projekt.repository;
+
+import com.example.kindergarten_projekt.model.TPostleitzahl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TPostleitzahlRepository extends JpaRepository<TPostleitzahl, String> {
+
+}
