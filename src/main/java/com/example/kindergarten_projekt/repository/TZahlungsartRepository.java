cda76a72ed31b184e7b937578dabab3261f65aac
EVGENY KVEST <evgenykvest@gmail.com>
2025-03-04 09:41:30 +0100

    TPostleitzahlRepository, TStandortRepository, TZahlungsartRepository erstellt

--- src/main/java/com/example/kindergarten_projekt/repository/TZahlungsartRepository.java
+++ src/main/java/com/example/kindergarten_projekt/repository/TZahlungsartRepository.java
@@ -2,6 +2,6 @@ package com.example.kindergarten_projekt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TZahlungsart extends JpaRepository<TZahlungsart, Integer> {
+public interface TZahlungsartRepository extends JpaRepository<TZahlungsartRepository, Integer> {
 
 }
