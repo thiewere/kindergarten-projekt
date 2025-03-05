bdef4dab234705bc4e31b7cf40223e866d8f13b7
EVGENY KVEST <evgenykvest@gmail.com>
2025-03-05 11:26:03 +0100

    AnwesenheitController, AnwesenheitService, AnwesenheitServiceImplement erstellt

--- src/main/java/com/example/kindergarten_projekt/repository/TAnwesenheitRepository.java
+++ src/main/java/com/example/kindergarten_projekt/repository/TAnwesenheitRepository.java
@@ -3,7 +3,19 @@ package com.example.kindergarten_projekt.repository;
 import com.example.kindergarten_projekt.model.TAnwesenheit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TAnwesenheitRepository extends JpaRepository<TAnwesenheit, Integer> {
 
+    List<TAnwesenheit> getTAnwesenheitByAwDatum(LocalDate awDatum);
 
+    Iterable<Integer> id(Integer id);
+
+    List<TAnwesenheit> removeTAnwesenheitByAwDatum(LocalDate awDatum);
+
+    List<TAnwesenheit> removeTAnwesenheitById(Integer id);
+
+    Date existsByAwDatum(LocalDate awDatum);
 }
--- src/main/java/com/example/kindergarten_projekt/repository/TStandortRepository.java
+++ src/main/java/com/example/kindergarten_projekt/repository/TStandortRepository.java
@@ -3,6 +3,10 @@ package com.example.kindergarten_projekt.repository;
 import com.example.kindergarten_projekt.model.TStandort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TStandortRepository extends JpaRepository<TStandort, Integer> {
+import java.util.List;
 
+public interface TStandortRepository extends JpaRepository<TStandort, Integer> {
+    List<TStandort> findByStandortName(String standortName);
+
+    List<TStandort> standortName(String standortName);
 }
